Simplified the saving process so it corrects a bug when scaling both batch size and total number of images.

ExoRIM/_train_master.py
@@ -3,6 +3,7 @@ from ExoRIM.data_generator import SimpleGenerator
 from datetime import datetime
 import tensorflow as tf
 import numpy as np
+import math
 
 
 class TrainMaster:
@@ -12,14 +13,12 @@ class TrainMaster:
     """
     def __init__(
             self,
-            generator=SimpleGenerator,
+            generator=SimpleGenerator(total_items=100, split=0.8),
             optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
             loss=MSE(),
             model_name="RIM",
             epochs=10,
-            total_images=100,
-            save_percent=0.1,
-            split=0.8,
+            images_saved=1,
             steps=12,  # number of steps for the reconstruction
             pixels=32,
             state_size=16,  # hidden state 2D size
@@ -41,20 +40,19 @@ class TrainMaster:
         self.epochs = epochs
         self.steps = steps
         self.phys = self.rim.physical_model
-        self.generator = generator(total_items=total_images, split=split)
+        self.generator = generator
         self.loss = loss
         self.optimizer = optimizer
 
         # utilities for plotting and saving weigths
-        self.train_traces, self.test_traces = self._pick_traces(total_images, save_percent, split)
+        self.train_traces, self.test_traces = self._pick_traces(images_saved)
         self.train_losses = []
         self.test_losses = []
         self.weight_grads = []
 
         self.trained = False
 
-    @staticmethod
-    def _pick_traces(total_images, save_percent, split):
+    def _pick_traces(self, images_saved):
         """
         This method selects a number of images that will be plotted and saved during training. It also picks out
         the moment during training when loss is displayed to the screen and saved for plotting.
@@ -64,10 +62,10 @@ class TrainMaster:
         :return: Index list of image to be saved in an epoch
         """
         np.random.seed(42)
-        possible_train_index = np.arange(1, int(total_images * split))
-        possible_test_index = np.arange(1, int(total_images * (1 - split)))
-        train_to_save = round(total_images * split * save_percent)
-        test_to_save = round(total_images * (1 - split) * save_percent)
+        possible_train_index = np.arange(1, self.generator.train_batches_in_epoch)
+        possible_test_index = np.arange(1, self.generator.test_batches_in_epoch)
+        train_to_save = int(math.ceil(self.generator.split * images_saved))
+        test_to_save = int(math.ceil(images_saved * (1 - self.generator.split)))
         train_traces = np.random.choice(possible_train_index, size=train_to_save, replace=False)
         test_traces = np.random.choice(possible_test_index, size=test_to_save, replace=False)
         return train_traces, test_traces

ExoRIM/model.py
@@ -202,7 +202,7 @@ class RIM(tf.keras.Model):
         with tf.GradientTape() as g:
             g.watch(x0)
             likelihood = self.log_likelihood(x0, y)
-        grad = self.gradient_instance_norm(g.gradient(likelihood, x0), )
+        grad = self.gradient_instance_norm(g.gradient(likelihood, x0))
         #grad = g.gradient(likelihood, x0)
         xt, ht = self.model(x0, h0, grad)
         outputs = tf.reshape(xt, xt.shape + [1])  # Plus one dimension for step stack

ExoRIM/train.py
@@ -11,22 +11,20 @@ import os
 class Training(TrainViz, TrainMaster):
     def __init__(
             self,
-            generator=SimpleGenerator,
+            generator=SimpleGenerator(total_items=1000, split=0.8, train_batch_size=50, test_batch_size=20),
             optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
             loss=MSE(),
             model_name="RIM",
             epochs=2,
-            total_images=100,
-            save_percent=0.01,
-            split=0.8,
+            images_saved=10,
             steps=12,  # number of steps for the reconstruction
             pixels=32,
             state_size=8,  # hidden state 2D size
             state_depth=2,  # Channel dimension of hidden state
             noise_std=0.1,
             num_cell_features=2,
-            step_trace=[3, 8, 11], # starting from 1
-            number_of_images=3
+            step_trace=[3, 8, 11], # starting from 0
+            number_of_images=1
     ):
         super(Training, self).__init__(
             step_trace=step_trace,
@@ -36,9 +34,7 @@ class Training(TrainViz, TrainMaster):
             loss=loss,
             model_name=model_name,
             epochs=epochs,
-            total_images=total_images,
-            save_percent=save_percent,
-            split=split,
+            images_saved=images_saved,
             steps=steps,  # number of steps for the reconstruction
             pixels=pixels,
             state_size=state_size,  # hidden state 2D size -- might be better if not param
@@ -59,7 +55,7 @@ class Training(TrainViz, TrainMaster):
         weight_grads = tape.gradient(cost_value, self.rim.trainable_weights)
         # prevent exploding gradient
         clipped_grads = [tf.clip_by_value(grads_i, -10, 10) for grads_i in weight_grads]
-        self.weight_grads.append(clipped_grads)
+        #self.weight_grads.append(clipped_grads)
         self.optimizer.apply_gradients(zip(clipped_grads, self.rim.trainable_weights))
         return output, loss_trace
 
@@ -79,19 +75,19 @@ class Training(TrainViz, TrainMaster):
         for image in self.generator.training_batch():
             output, train_trace_loss = self._train_batch(image, train_loss_trace)
             if self.generator.train_index in self.train_traces:
-                self.train_losses.append(np.mean(train_trace_loss))
+                self.train_losses.append(np.mean(train_trace_loss)/self.generator.train_batch_size)
                 self.snap(output, image, epoch, state="train")
                 if verbose > 0:
-                    print(f"epoch {epoch + 1}: training loss = {np.mean(train_trace_loss)}")
+                    print(f"epoch {epoch + 1}: training loss = {np.mean(train_trace_loss)/self.generator.train_batch_size}")
                 train_loss_trace = []  # reset the trace
         # test
         for image in self.generator.test_batch():
             output, test_loss_trace = self._test_batch(image, test_loss_trace)
             if self.generator.test_index in self.test_traces:
-                self.test_losses.append(np.mean(test_loss_trace))  # we plot the mean of the loss
-                self.snap(output, image, state="test")
+                self.test_losses.append(np.mean(test_loss_trace)/self.generator.test_batch_size)  # we plot the mean of the loss
+                self.snap(output, image, epoch, state="test")
                 if verbose > 0:
-                    print(f"epoch {epoch + 1}: test loss = {np.mean(test_loss_trace)}")
+                    print(f"epoch {epoch + 1}: test loss = {np.mean(test_loss_trace)/self.generator.test_batch_size}")
                 test_loss_trace = []  # reset the trace
 
     def train_weights(self):

ExoRIM/training_visualization.py
@@ -5,6 +5,9 @@ import numpy as np
 from ExoRIM.definitions import lossdir, image_dir
 from ExoRIM._train_master import TrainMaster
 from celluloid import Camera
+import warnings
+import matplotlib.cbook
+warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
 plt.rcParams["figure.figsize"] = (10, 10)
 
 
@@ -64,11 +67,13 @@ class TrainViz(TrainMaster):
             gs = self.gs_train
             images = self._train_images
             losses = self.train_losses
+            loss_stop = epoch * self.generator.train_batches_in_epoch/fig
         else:
             gs = self.gs_test
             fig = self.generator.test_index
             images = self._test_images
             losses = self.test_losses
+            loss_stop = epoch * self.generator.test_batches_in_epoch / fig
         loss_ax = self.movie_figs[state][fig].add_subplot(gs[-1, :])
         cbar_ax = self.movie_figs[state][fig].add_subplot(gs[:-1, -1])
         self.movie_figs[state][fig].colorbar(
@@ -76,8 +81,8 @@ class TrainViz(TrainMaster):
             cax=cbar_ax
         )
         loss_ax.set_xlabel("Epochs")
-        loss_ax.set_ylabel("Loss")
-        loss_x_axis = np.linspace(0, epoch * self.generator.total_items/fig, num=len(losses))
+        loss_ax.set_ylabel("Average Loss (over all samples)")
+        loss_x_axis = np.linspace(0, loss_stop, num=len(losses))
         loss_ax.plot(loss_x_axis, losses, "k-")
 
         for row, image in enumerate(images):