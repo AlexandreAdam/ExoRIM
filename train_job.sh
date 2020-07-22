6c634c68740b6b691d770ba4dc4fd07235ed3e2b
Alexandre Adam <aadam@beluga4.int.ets1.calculquebec.ca>
2020-07-22 17:42:48 -0400

    Updated beluga train script

--- train_job.sh
+++ train_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --gres=gpu:2			# request GPU "generic resource"
-#SBATCH --cpus-per-task=3		# maximum cpu per task is 3.5 per gpus
-#SBATCH --mem=3G			# memory per node
-#SBATCH --time=0-05:00			# time (DD-HH:MM)
+#SBATCH --cpus-per-task=2		# maximum cpu per task is 3.5 per gpus
+#SBATCH --mem=8G			# memory per node
+#SBATCH --time=0-1:00			# time (DD-HH:MM)
 #SBATCH --account=def-lplevass
 #SBATCH --job-name=Train_ExoRIM
 #SBATCH --output=%x-%j.out
@@ -14,4 +14,4 @@ pip install --no-index -r cc_requirements.txt
 pip install /home/aadam/scratch/ExoRIM/dist/ExoRim-0.1-py3-none-any.whl
 module load cuda cudnn 
 python -W ignore::DeprecationWarning
-python main.py -n 5000 -s 0.8 -t 4 --holes 20 -p 20 -e 1000 -b 32 --out_save_mod 300 --SNR 50 --format "txt" --longest_baseline 20
+python main.py -n 1000 -s 0.8 -t 4 --holes 9 -p 50 -e 1000 -b 16 --index_save_mod 500 --SNR 100 --format "txt" --longest_baseline 6 --mask_variance 1 --plate_scale 3.2
