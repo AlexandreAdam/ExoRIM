Catched some bug in main. Fixed them and added a scaling parameter as well as a floor to define a log window

ExoRIM/rim.py
@@ -9,8 +9,11 @@ import os
 
 
 class RIM:
-    def __init__(self, physical_model, hyperparameters=default_hyperparameters, dtype=dtype, weight_file=None):
+    def __init__(self, physical_model, hyperparameters=default_hyperparameters, dtype=dtype, weight_file=None,
+                 noise_floor=1e-8, scaling_factor=10**5):
         self._dtype = dtype
+        self.noise_floor = noise_floor
+        self.scaling_factor = scaling_factor
         self.hyperparameters = hyperparameters
         self.channels = hyperparameters["channels"]
         self.pixels = hyperparameters["pixels"]
@@ -25,19 +28,18 @@ class RIM:
             self.model.load_weights(weight_file)
         self.physical_model = physical_model
 
-    @staticmethod
     @tf.function
-    def link_function(y):
-        return tf.math.log(y + 1e-8)
+    def link_function(self, y):
+        return tf.math.log(y + self.noise_floor)
 
     @staticmethod
     @tf.function
     def inverse_link_function(eta):
         return tf.math.exp(eta)
 
-    @tf.function
+    # @tf.function
     def grad_scaling(self, grad):
-        return tf.clip_by_value(1/self.physical_model.SNR**2 * grad, -10, 10)
+        return tf.clip_by_value(1/self.physical_model.SNR**2/self.scaling_factor/self.pixels * grad, -10, 10)
 
     def __call__(self, X):
         return self.call(X)
@@ -212,8 +214,8 @@ class RIM:
 
             if test_dataset is not None:
                 with test_writer.as_default():
-                    for X, Y in test_dataset:  # this dataset should not be batched, so this for loop has 1 iteration
-                        test_eta_output, _ = self.call(X)  # investigate why predict returns NaN scores and output
+                    for batch, (X, Y) in test_dataset:  # this dataset should not be batched, so this for loop has 1 iteration
+                        test_eta_output, _ = self.call(X)
                         test_output = self.inverse_link_function(test_eta_output)
                         test_cost = cost_function(test_eta_output, self.link_function(Y))
                         test_cost += tf.reduce_sum(self.model.losses)

ExoRIM/simulated_data.py
@@ -268,11 +268,13 @@ class CenteredImagesGenerator:
             self,
             physical_model: PhysicalModel,
             total_items_per_epoch,
+            scaling_factor,
             channels=1,
             highest_contrast=0.5,
             max_point_sources=5,
             fixed=False
     ):
+        self.scaling_factor = scaling_factor
         self.physical_model = physical_model
         self.total_items_per_epoch = total_items_per_epoch
         self.channels = channels
@@ -302,7 +304,7 @@ class CenteredImagesGenerator:
         xx, yy = np.meshgrid(image_coords, image_coords)
         image = np.zeros_like(xx)
         rho_squared = (xx - xp) ** 2/a**2 + (yy - yp) ** 2/b**2
-        image += intensity * np.exp(-0.5 * (rho_squared / sigma ** 2))
+        image += intensity * np.exp(-0.5 * (rho_squared**2 / sigma ** 2))
         return image
 
     def circular_psf(self, sigma, intensity, xp, yp, a=1, b=1):
@@ -333,14 +335,14 @@ class CenteredImagesGenerator:
         # images += 1e-5 * np.random.random(size=images.shape) + 1e-4  # background
         nps = self._nps()[0]
         width = self._width(nps)
-        intensity = 1 - self._contrasts(nps)
+        intensity = self.scaling_factor*(1 - self._contrasts(nps))
         coordinates = self._coordinates(nps)
         elongation = self._elongation(nps)
         for i in range(nps):
             # image += self.circular_psf(width[i], intensity[i], *coordinates[i], *elongation[i])
             image += self.gaussian_psf_convolution(width[i], intensity[i], *coordinates[i], *elongation[i])
         # image = self.recenter(image)
-        image = self.normalize(image, minimum=0, maximum=1)
+        # image = self.normalize(image, minimum=0, maximum=1)
         image = np.reshape(image, newshape=[self.pixels, self.pixels, 1])
         return image
 

ExoRIM/utilities.py
@@ -135,7 +135,9 @@ def create_dataset_from_generator(
         batch_size=50,
         highest_contrast=0.5,
         max_point_source=10,
-        fixed=False
+        fixed=False,
+        scaling_factor=10**5,
+        seed=None
 ):
     gen = CenteredImagesGenerator(
         physical_model=physical_model,
@@ -143,8 +145,11 @@ def create_dataset_from_generator(
         channels=1,
         highest_contrast=highest_contrast,
         max_point_sources=max_point_source,
-        fixed=fixed
+        fixed=fixed,
+        scaling_factor=scaling_factor
     )
+    if fixed and seed is not None:
+        gen.epoch = seed
     pixels = physical_model.pixels
     shapes = (tf.TensorShape([None]), tf.TensorShape([pixels, pixels, 1]))
     dataset = tf.data.Dataset.from_generator(gen.generator, output_types=(mycomplex, dtype), output_shapes=shapes)

main.py
@@ -1,8 +1,7 @@
 from ExoRIM import RIM, MSE, PhysicalModel
-from ExoRIM.loss import KLDivergence, MAE
-from ExoRIM.simulated_data import CenteredImagesv1, OffCenteredBinaries, CenteredCircle
+from ExoRIM.loss import MAE
 from preprocessing.simulate_data import create_and_save_data
-from ExoRIM.definitions import mas2rad, dtype
+from ExoRIM.definitions import dtype
 from ExoRIM.utilities import create_dataset_from_generator, replay_dataset_from_generator
 from argparse import ArgumentParser
 from datetime import datetime
@@ -54,6 +53,8 @@ if __name__ == "__main__":
     parser.add_argument("-e", "--max_epoch", type=int, default=10, help="Maximum number of epoch")
     parser.add_argument("--index_save_mod", type=int, default=20, help="Image index to be saved")
     parser.add_argument("--epoch_save_mod", type=int, default=1, help="Epoch at which to save images")
+    parser.add_argument("--scaling_factor", type=float, default=10**5, help="Maximum intensity, sets the log domain for the model input")
+    parser.add_argument("--noise_floor", type=float, default=1, help="Intensity noise floor")
     parser.add_argument("--format", type=str, default="png", help="Format with which to save image, either png or txt")
     parser.add_argument("--fixed", action="store_true", help="Keeps the dataset fix for each epochs to monitor progress")
     args = parser.parse_args()
@@ -61,14 +62,7 @@ if __name__ == "__main__":
     print(f"id = {date}")
     with open("hyperparameters.json", "r") as f:
         hyperparameters = json.load(f)
-    # train_meta = CenteredImagesv1(
-    #     total_items=args.number_images,
-    #     pixels=hyperparameters["pixels"]
-    # )
-    test_meta = CenteredImagesv1(
-        total_items=int(args.number_images * (1 - args.split)),
-        pixels=hyperparameters["pixels"]
-    )
+
     hyperparameters["batch"] = args.batch
     hyperparameters["date"] = date
     # metrics only support grey scale images
@@ -121,15 +115,22 @@ if __name__ == "__main__":
         wavelength=args.wavelength,
         SNR=args.SNR
     )
-    rim = RIM(physical_model=phys, hyperparameters=hyperparameters)
-    # train_dataset = create_datasets(train_meta, rim, dirname=train_dir, batch_size=args.batch, index_save_mod=args.index_save_mod, format=args.format)
+    rim = RIM(physical_model=phys, hyperparameters=hyperparameters, noise_floor=args.noise_floor, scaling_factor=args.scaling_factor)
     train_dataset = create_dataset_from_generator(
         physical_model=phys,
         item_per_epoch=args.number_images,
         batch_size=args.batch,
-        fixed=args.fixed
+        fixed=args.fixed,
+        scaling_factor=args.scaling_factor,
     )
-    test_dataset = create_datasets(test_meta, rim, dirname=test_dir, index_save_mod=args.index_save_mod, format=args.format)
+    test_dataset = create_dataset_from_generator(
+        physical_model=phys,
+        item_per_epoch=int(args.number_images * (1 - args.split)),
+        batch_size=int(args.number_images * (1 - args.split)),
+        fixed=args.fixed,
+        scaling_factor=args.scaling_factor,
+        seed=31415926
+    )
     cost_function = MSE()
     learning_rate_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
         **hyperparameters["learning rate"],
@@ -173,4 +174,13 @@ if __name__ == "__main__":
         format="txt",
         index_mod=args.index_save_mod,
         epoch_mod=args.epoch_save_mod
+    )
+    replay_dataset_from_generator(
+        test_dataset,
+        epochs=rim.hyperparameters["epoch"],
+        dirname=test_dir,
+        fixed=args.fixed,
+        format="txt",
+        index_mod=1,
+        epoch_mod=1
     )